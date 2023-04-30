70b6a2b131dd23d6988d8fe546b8fd5d0b9809b2
Rodrigo Bueno <buenodevv@gmail.com>
2023-04-30 10:23:15 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Logo} from "./styles";
+import logoImg from '@assets/logo.png'
+import { CaretLeft } from 'phosphor-react-native'
+
+
+export function Header() {
+    return (
+        
+        <Container>
+            <CaretLeft color="#fff" size={32}/>
+           <Logo source={logoImg}/>
+        </Container>
+    )
+}
