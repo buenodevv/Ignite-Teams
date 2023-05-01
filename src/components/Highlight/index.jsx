22114ef298b14fac41543bee3277994207f042c2
Rodrigo Bueno <buenodevv@gmail.com>
2023-05-01 07:42:10 -0300

    componente Highlight added

---
+++ src/components/Highlight/index.jsx
@@ -0,0 +1,14 @@
+import { Container, Title, Subtitle } from "./styles"
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export function Highlight() {
+    return (
+        <Container>
+            <Title></Title>
+            <Subtitle></Subtitle>
+        </Container>
+    )
+}
+
---
+++ src/components/Highlight/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View `
+    width: 100% ;
+    margin: 32px;
+`;
+
+export const Title = styled.Text`
+    text-align: center;
+
+    font-size: ${({ theme }) => theme.FONT_SIZE.GG}px;
+    font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+    color: ${({ theme }) => theme.CORES.BRANCO};
+    
+`
+export const Subtitle = styled.Text`
+    text-align: center;
+
+    font-size: ${({ theme }) => theme.FONT_SIZE.DM}px;
+    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+    color: ${({ theme }) => theme.CORES.GRAY_400};
+    
+`
