1a4bbaa54b2c2327eae67876be2099cfbb382a2d
Rodrigo Bueno <buenodevv@gmail.com>
2023-05-18 06:37:03 -0300

    Class AppError implemented

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import { Header } from '@components/Header'
 import { Container, Content, Icon } from './styles'
+import { AppError } from '@utils/AppError'
 import { Button } from '@components/Button'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
@@ -16,11 +18,20 @@ export function NewGroup() {
 
     async function handleNew() {
         try {
+            if ( group.trim().length === 0) {
+                return Alert.alert('Novo Grupo', 'Informe o nome da turma!')
+            }
+
             await groupCreate(group)
             navigation.navigate("players", { group });
 
         } catch (error) {
-            console.log(error)
+            if(error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message);
+            }else {
+                Alert.alert('Novo grupo', 'Não foi posssivel criar um novo grupo.');
+                console.log(error)
+            }
         }
     }
 return (
